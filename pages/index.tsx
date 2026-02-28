d7366f135d475fa7ebaeaa3ace809c4aac1d65c9
pappdavid <david@webinform.hu>
2026-02-28 21:56:29 +0100

    Add home page redirect

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import React, { useEffect } from 'react';
+import { useRouter } from 'next/router';
+
+export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace('/dashboard');
+  }, [router]);
+
+  return null;
+}
